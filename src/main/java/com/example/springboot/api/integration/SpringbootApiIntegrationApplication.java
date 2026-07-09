55359fdf47b204274ee715c9ddee01340afb3c67
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-07-09 16:12:10 +0530

    initial commit

---
+++ src/main/java/com/example/springboot/api/integration/SpringbootApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.api.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/api/integration/SpringbootApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.api.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
