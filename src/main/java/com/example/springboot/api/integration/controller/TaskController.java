0a58f11c5fb559c514621a7aab13baeeb567c621
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-07-10 15:54:43 +0530

    refactor TaskController to update API endpoint paths and remove redundant '/api' prefix

--- src/main/java/com/example/springboot/api/integration/controller/TaskController.java
+++ src/main/java/com/example/springboot/api/integration/controller/TaskController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TaskController {
     @Autowired
     private TaskService taskService;
